6e85f5dc14d6f0590ad187ab287125fcc6755ce4
innaroshchepa <62202381+innaroshchepa@users.noreply.github.com>
2024-04-14 14:01:51 +0200

    create

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,38 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+  constructor(protected key: Key) {}
+  abstract openDoor(key: Key): void;
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) this.door = true;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +42,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { };
